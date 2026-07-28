bbb8f9aa3af3e5dc7f6553e62c9796926439b9fd
Madhav Gupta <27madhav27@gmail.com>
2026-07-28 15:31:59 -0400

    fix: declare lenis dep and set Vercel Portfolio build root

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import Lenis from '@studio-freight/lenis';
+import Lenis from 'lenis';
 import Navbar from './Navbar';
 import Hero from './Hero';
 import Skills from './Skills';
